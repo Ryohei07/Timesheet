d8fc53125bfbb480a0b5ae375c2a6cdbe4cc765d
Ryohei07 <83806681+Ryohei07@users.noreply.github.com>
2021-06-06 17:21:38 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+      <div className="inputA">
+        <button>出社</button>
+      </div>
+      <div className="inputB">
+        <button>退社</button>
+      </div>
+    </>
+  );
+}
