75bea0c72b4689ae637b182fb739f1699891f0c5
Ryohei07 <83806681+Ryohei07@users.noreply.github.com>
2021-06-06 18:20:51 +0900

    work0606_01

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import "./styles.css";
 export default function App() {
   return (
     <>
+      <div className="outputArea">
+        <p>出社時間：</p>
+        <p>退社時間：</p>
+      </div>
       <div className="inputA">
         <button>出社</button>
       </div>
